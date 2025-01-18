1521b71d61d7c5ffd74536080a96a27717c74995
tikhon <tikhonshamruk@gmail.com>
2025-01-18 23:31:52 +0300

    add header section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom'
+import { ROUTES } from '../../utils/routes'
+import './Footer.css'
+import LOGO from "../../assets/header/LOGO 1.svg";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='footer'>
+      <div className='footer-logo'>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO}></img>
+        </Link>
+      </div>
+    </section>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,43 @@
 import { Link } from "react-router-dom"
 import { ROUTES } from "../../utils/routes"
 import './Header.css'
+import LOGO from "../../assets/header/LOGO 1.svg";
+import LOGO_QUEST from "../../assets/header/Vector.svg"; 
+import LOOP from "../../assets/header/Vector (1).svg"
+import HEART from "../../assets/header/Vector (2).svg"
+import CART from "../../assets/header/Vector (3).svg"
+
 
 const Header = () => {
   return (
-    <div className="header">
+    <section className="header">
         <div className="logo">
-            <Link to={ROUTES.HOME}></Link>
+            <Link to={ROUTES.HOME}>
+              <img src={LOGO} alt="stuff" />
+            </Link>
         </div>
-        Header
-    </div>
+        <div className="logo-guest">
+          <div className="logo-guest_pic">
+            <img src={LOGO_QUEST} alt="" />
+          </div>
+          <span className="regular">Yana Tamkovich</span>
+        </div>
+        
+        <form className="header-form">
+            <img src={LOOP} alt="loop" />
+            <input type="search" className="regular" placeholder="Search for anything..." />
+        </form>
+
+        <div className="header-items">
+          <Link to={ROUTES.HOME}>
+            <img src={HEART} alt="heart" />
+          </Link>
+          <Link className="header-items_link" to={ROUTES.CART}>
+            <img src={CART} alt="carts" />
+            <span className="header-items_carts">5</span>
+          </Link>
+        </div>
+    </section>
   )
 }
 
